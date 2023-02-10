a7bcaf96861a2338501ee92c89f0f1092dfa3396
dohung02 <hungdpps19239@fpt.edu.vn>
2023-02-10 22:20:42 +0700

    xóa file source

--- Website-Ban-Cay-Canh/src/main/java/com/datamining/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.datamining.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.datamining.entity.Product;
-
-public interface ProductDao extends JpaRepository<Product, Integer> {
-
-}
--- Website-Ban-Cay-Canh/src/main/resources/static/js/account-scrpit.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('file-input');
-const image = document.getElementById('img-preview');
-
-input.addEventListener('change', (e) => {
-    if (e.target.files.length) {
-        const src = URL.createObjectURL(e.target.files[0]);
-        image.src = src;
-    }
-});
